d59b51a0d4cc3f4d93148fb9ade52d74cf39e9dc
David Gaither <gaither@protonmail.com>
2024-01-31 14:45:42 -0500

    Fibonacci.js completed and tested

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,11 @@
 function fibonacci(num) {
-  return 0;
+  let [numOne, numTwo, fib] = [0, 1, 0];
+  for (let i = 1; i < num; i++) {
+    fib = numOne + numTwo
+    numOne = numTwo
+    numTwo = fib 
+  }
+return fib
 }
 
 module.exports = fibonacci;
